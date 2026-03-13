f812609297d420391ac7a73c1ba8977a24430f72
YunliEricHsieh <47656312+YunliEricHsieh@users.noreply.github.com>
2026-03-13 14:45:55 +0100

    Updated

--- Code/matlab/flux_sampling.m
+++ Code/matlab/flux_sampling.m
@@ -2,34 +2,28 @@ changeCobraSolver('gurobi','all');
 changeCobraSolverParams('LP', 'feasTol', 1e-6);
 changeCobraSolverParams('QP', 'feasTol', 1e-6);
 
-%% load different type of models
-o_auto_model = readCbModel('Data/pciCre1355/NDLadpraw_Autotrophic_Rep1.xml');
-o_mixo_model = readCbModel('Data/pciCre1355/NDLadpraw_Mixotrophic_Rep1.xml');
-o_hetero_model = readCbModel('Data/pciCre1355/NDLadpraw_Heterotrophic_Rep1.xml');
+ncpu = 20;
+if isempty(gcp('nocreate'))
+    parpool(ncpu);
+    parfevalOnAll(@maxNumCompThreads, 0, 1); 
+end
 
-auto_model = o_auto_model;
-mixo_model = o_mixo_model;
-hetero_model = o_hetero_model;
+%% load different type of models
+auto_model   = readCbModel('Data/pciCre1355/NDLadpraw_Autotrophic_Rep1.xml');
+mixo_model   = readCbModel('Data/pciCre1355/NDLadpraw_Mixotrophic_Rep1.xml');
+hetero_model = readCbModel('Data/pciCre1355/NDLadpraw_Heterotrophic_Rep1.xml');
 
 % find exchange and uptake reactions
 ex_rxns = auto_model.rxns(findExcRxns(auto_model));
-
-% target only uptake reactions
 upt_rxns = {};
 
 for i = 1:numel(ex_rxns)
-
-    col = find(ismember(auto_model.rxns,ex_rxns{i}));
-
-    % also remove the demand reactions from the list
-    if sum(auto_model.S(:,col)) == 1 & ~contains(ex_rxns{i}, 'DM') & contains(ex_rxns{i}, 'EX')
-
-        upt_rxns = [upt_rxns; ex_rxns(i)];
-
-    elseif sum(auto_model.S(:,col)) > 1
-
-        fprintf('Need to check the stoichiometric matrix for col: %d\n',col)
-        return
+    col = find(strcmp(auto_model.rxns, ex_rxns{i}));
+    % Use nnz (number of non-zero metabolites) to safely identify exchanges
+    if nnz(auto_model.S(:,col)) == 1 && startsWith(ex_rxns{i}, 'EX_') && ~startsWith(ex_rxns{i}, 'DM_')
+        upt_rxns{end+1, 1} = ex_rxns{i};
+    elseif nnz(auto_model.S(:,col)) > 1
+        fprintf('Warning: Check stoichiometry for col: %d (%s)\n', col, ex_rxns{i});
     end
 end
 
@@ -39,35 +33,23 @@ hetero_upt_id = find(ismember(hetero_model.rxns, upt_rxns));
 mixo_upt_id = find(ismember(mixo_model.rxns, upt_rxns));
 
 % remove the upper bound constraint of uptake reactions
-for i = 1:numel(upt_rxns)
-    if auto_model.ub(auto_upt_id(i)) ~= 0
-        auto_model.ub(auto_upt_id(i)) = 1000;
-    end
+auto_model.ub(auto_upt_id(auto_model.ub(auto_upt_id) ~= 0)) = 1000;
+hetero_model.ub(hetero_upt_id(hetero_model.ub(hetero_upt_id) ~= 0)) = 1000;
+mixo_model.ub(mixo_upt_id(mixo_model.ub(mixo_upt_id) ~= 0)) = 1000;
 
-    if hetero_model.ub(hetero_upt_id(i)) ~= 0
-        hetero_model.ub(hetero_upt_id(i)) = 1000;
-    end
-
-    if mixo_model.ub(mixo_upt_id(i)) ~= 0
-        mixo_model.ub(mixo_upt_id(i)) = 1000;
-    end
-end
-
+% find optimize growth
 auto_opt = optimizeCbModel(auto_model);
 hetero_opt = optimizeCbModel(hetero_model);
 mixo_opt = optimizeCbModel(mixo_model);
 
-auto_index = find(auto_model.c == 1);
-hetero_index = find(hetero_model.c == 1);
-mixo_index = find(mixo_model.c == 1);
+auto_model.lb(auto_index) = 0.9 * auto_opt.f;
+auto_model.ub(auto_index) = 0.9 * auto_opt.f;
 
-auto_model.lb(auto_index) = 0.9*auto_opt.f;
-hetero_model.lb(hetero_index) = 0.9*hetero_opt.f;
-mixo_model.lb(mixo_index) = 0.9*mixo_opt.f;
+hetero_model.lb(hetero_index) = 0.9 * hetero_opt.f;
+hetero_model.ub(hetero_index) = 0.9 * hetero_opt.f;
 
-auto_model.ub(auto_index) = 0.9*auto_opt.f;
-hetero_model.ub(hetero_index) = 0.9*hetero_opt.f;
-mixo_model.ub(mixo_index) = 0.9*mixo_opt.f;
+mixo_model.lb(mixo_index) = 0.9 * mixo_opt.f;
+mixo_model.ub(mixo_index) = 0.9 * mixo_opt.f;
 
 [a_sampleStruct, a_mixedFraction] = gpSampler(auto_model);
 [h_sampleStruct, h_mixedFraction] = gpSampler(hetero_model);
@@ -76,12 +58,7 @@ mixo_model.ub(mixo_index) = 0.9*mixo_opt.f;
 %% create the rxn index
 % find the metabolic reactions
 filterRxns = @(model) model.rxns(~contains(model.rxns, {'EX_','draw','prot_pool'}));
-
-auto_rxns   = filterRxns(auto_model);
-mixo_rxns   = filterRxns(mixo_model);
-hetero_rxns = filterRxns(hetero_model);
-
-rxn_list = intersect(intersect(auto_rxns, mixo_rxns), hetero_rxns);
+rxn_list = intersect(intersect(filterRxns(auto_model), filterRxns(mixo_model)), filterRxns(hetero_model));
 
 % remove the block reaction in rxn_list
 auto_FVA = readtable('Results/FVA/auto_FVA_10p.csv');
@@ -91,7 +68,6 @@ hetero_FVA = readtable('Results/FVA/hetero_FVA_10p.csv');
 % identify blocked reactions in the models
 getBlocked = @(FVA) FVA.RxnID(FVA.minFlux == 0 & FVA.maxFlux == 0);
 blocked_all = unique([getBlocked(auto_FVA); getBlocked(mixo_FVA); getBlocked(hetero_FVA)]);
-
 rxn_list = setdiff(rxn_list, blocked_all);
 
 % create reaction index cell array
@@ -103,150 +79,52 @@ rxn_list = rxn_list(valid_idx);
 rxn_cell = rxn_cell(valid_idx);
 
 % find reversible reactions (forward and backward)
-is_rev = contains(rxn_list, '_REV');
+is_rev = endsWith(rxn_list, '_REV');
 rerxn  = rxn_list(is_rev);
 rerxn_cell = rxn_cell(is_rev);
 
 forward_rxns = strrep(rerxn, '_REV', '');
 forward_cells = rxn_cell(ismember(rxn_list, forward_rxns));
 
-% combine the lists
-re_rxn_cells = [forward_cells; rerxn_cell];
 re_rxn_list = [forward_rxns; rerxn];
+re_rxn_cells = [forward_cells; rerxn_cell];
 
-% filter out the reversible reactions
-rxn_list = rxn_list(~ismember(rxn_list, re_rxn_list));
-rxn_cell = rxn_cell(~ismember(rxn_cell, re_rxn_cells));
+% irreversible reactions
+rxn_list_ir = setdiff(rxn_list, re_rxn_list, 'stable');
+rxn_cell_ir = setdiff(rxn_cell, re_rxn_cells, 'stable');
 
-%% find the flux distribution
-% auto_model
-meanFlux = cell(height(rxn_cell),1);
-stdFlux = cell(height(rxn_cell),1);
-
-a_fluxSamples = a_sampleStruct.points;
-for i = 1:height(rxn_cell)
-    rxnIndex = find(ismember(auto_model.rxns, rxn_list{i}));
-
-    targetFluxDistribution = a_fluxSamples(rxnIndex, :);
-    meanFlux{i} = mean(targetFluxDistribution);
-    stdFlux{i} = std(targetFluxDistribution);
-
-    clear targetFluxDistribution
-end
-
-tmp_table = cell2table([rxn_cell, rxn_list, meanFlux, stdFlux]);
-tmp_table.Properties.VariableNames = {'RxnIndex', 'RxnID', 'meanFlux', 'stdFlux'};
-
-writetable(tmp_table, 'Results/flux_sampling/auto_sampling.csv');
-
-% hetero_model
-meanFlux = cell(height(rxn_cell),1);
-stdFlux = cell(height(rxn_cell),1);
-
-h_fluxSamples = h_sampleStruct.points;
-for i = 1:height(rxn_cell)
-
-    rxnIndex = find(ismember(hetero_model.rxns, rxn_list{i}));
-
-    targetFluxDistribution = h_fluxSamples(rxnIndex, :);
-    meanFlux{i} = mean(targetFluxDistribution);
-    stdFlux{i} = std(targetFluxDistribution);
-
-    clear targetFluxDistribution
-end
-
-tmp_table = cell2table([rxn_cell, rxn_list, meanFlux, stdFlux]);
-tmp_table.Properties.VariableNames = {'RxnIndex', 'RxnID', 'meanFlux', 'stdFlux'};
-
-writetable(tmp_table, 'Results/flux_sampling/hetero_sampling.csv');
-
-% mixo_model
-meanFlux = cell(height(rxn_cell),1);
-stdFlux = cell(height(rxn_cell),1);
-
-m_fluxSamples = m_sampleStruct.points;
-for i = 1:height(rxn_cell)
-
-    rxnIndex = find(ismember(mixo_model.rxns, rxn_list{i}));
-
-    targetFluxDistribution = m_fluxSamples(rxnIndex, :);
-    meanFlux{i} = mean(targetFluxDistribution);
-    stdFlux{i} = std(targetFluxDistribution);
-
-    clear targetFluxDistribution
-end
-
-tmp_table = cell2table([rxn_cell, rxn_list, meanFlux, stdFlux]);
-tmp_table.Properties.VariableNames = {'RxnIndex', 'RxnID', 'meanFlux', 'stdFlux'};
-
-writetable(tmp_table, 'Results/flux_sampling/mixo_sampling.csv');
+%% find the flux distribution for irreversible reactions
+process_irr_samples(auto_model, a_sampleStruct.points, rxn_cell_ir, rxn_list_ir, 'Results/flux_sampling/auto_sampling.csv');
+process_irr_samples(hetero_model, h_sampleStruct.points, rxn_cell_ir, rxn_list_ir, 'Results/flux_sampling/hetero_sampling.csv');
+process_irr_samples(mixo_model, m_sampleStruct.points, rxn_cell_ir, rxn_list_ir, 'Results/flux_sampling/mixo_sampling.csv');
 
 %% find the flux distribution for reversible reactions
-% auto_model
-meanFlux = cell(height(rerxn),1);
-stdFlux = cell(height(rerxn),1);
+process_rev_samples(auto_model, a_sampleStruct.points, forward_cells, forward_rxns, rerxn, 'Results/flux_sampling/auto_sampling_re.csv');
+process_rev_samples(hetero_model, h_sampleStruct.points, forward_cells, forward_rxns, rerxn, 'Results/flux_sampling/hetero_sampling_re.csv');
+process_rev_samples(mixo_model, m_sampleStruct.points, forward_cells, forward_rxns, rerxn, 'Results/flux_sampling/mixo_sampling_re.csv');
 
-a_fluxSamples = a_sampleStruct.points;
-for i = 1:height(rerxn)
-    rxnIndex_b = find(ismember(auto_model.rxns, rerxn{i}));
-    rxnIndex_f = find(ismember(auto_model.rxns, strrep(rerxn{i}, '_REV', '')));
-
-    targetFluxDistribution_f = a_fluxSamples(rxnIndex_f, :);
-    targetFluxDistribution_b = a_fluxSamples(rxnIndex_b, :);
-
-    meanFlux{i} = mean(abs(targetFluxDistribution_f - targetFluxDistribution_b));
-    stdFlux{i} = std(abs(targetFluxDistribution_f - targetFluxDistribution_b));
-
-    clear targetFluxDistribution_f targetFluxDistribution_b
+%% locat function
+function process_irr_samples(model, samples, rxn_ids, rxn_names, out_path)
+    [~, idx] = ismember(rxn_names, model.rxns);
+    
+    % Calculate mean/std across rows (dimension 2) instantly
+    meanFlux = mean(samples(idx, :), 2);
+    stdFlux  = std(samples(idx, :), 0, 2);
+    
+    T = table(rxn_ids, rxn_names, meanFlux, stdFlux, 'VariableNames', {'RxnIndex', 'RxnID', 'meanFlux', 'stdFlux'});
+    writetable(T, out_path);
 end
 
-tmp_table = cell2table([forward_cells, forward_rxns, meanFlux, stdFlux]);
-tmp_table.Properties.VariableNames = {'RxnIndex', 'RxnID', 'meanFlux', 'stdFlux'};
-
-writetable(tmp_table, 'Results/flux_sampling/auto_sampling_re.csv');
-
-% hetero_model
-meanFlux = cell(height(rerxn),1);
-stdFlux = cell(height(rerxn),1);
-
-h_fluxSamples = h_sampleStruct.points;
-for i = 1:height(rerxn)
-    rxnIndex_b = find(ismember(hetero_model.rxns, rerxn{i}));
-    rxnIndex_f = find(ismember(hetero_model.rxns, strrep(rerxn{i}, '_REV', '')));
-
-    targetFluxDistribution_f = h_fluxSamples(rxnIndex_f, :);
-    targetFluxDistribution_b = h_fluxSamples(rxnIndex_b, :);
-
-    meanFlux{i} = mean(abs(targetFluxDistribution_f - targetFluxDistribution_b));
-    stdFlux{i} = std(abs(targetFluxDistribution_f - targetFluxDistribution_b));
-
-    clear targetFluxDistribution_f targetFluxDistribution_b
+function process_rev_samples(model, samples, f_ids, f_names, b_names, out_path)
+    [~, idx_f] = ismember(f_names, model.rxns);
+    [~, idx_b] = ismember(b_names, model.rxns);
+    
+    % Subtract full matrices and take absolute value instantly
+    net_flux = abs(samples(idx_f, :) - samples(idx_b, :));
+    
+    meanFlux = mean(net_flux, 2);
+    stdFlux  = std(net_flux, 0, 2);
+    
+    T = table(f_ids, f_names, meanFlux, stdFlux, 'VariableNames', {'RxnIndex', 'RxnID', 'meanFlux', 'stdFlux'});
+    writetable(T, out_path);
 end
-
-tmp_table = cell2table([forward_cells, forward_rxns, meanFlux, stdFlux]);
-tmp_table.Properties.VariableNames = {'RxnIndex', 'RxnID', 'meanFlux', 'stdFlux'};
-
-writetable(tmp_table, 'Results/flux_sampling/hetero_sampling_re.csv');
-
-% mixo_model
-meanFlux = cell(height(rerxn),1);
-stdFlux = cell(height(rerxn),1);
-
-m_fluxSamples = m_sampleStruct.points;
-for i = 1:height(rerxn)
-    rxnIndex_b = find(ismember(mixo_model.rxns, rerxn{i}));
-    rxnIndex_f = find(ismember(mixo_model.rxns, strrep(rerxn{i}, '_REV', '')));
-
-    targetFluxDistribution_f = m_fluxSamples(rxnIndex_f, :);
-    targetFluxDistribution_b = m_fluxSamples(rxnIndex_b, :);
-
-    meanFlux{i} = mean(abs(targetFluxDistribution_f - targetFluxDistribution_b));
-    stdFlux{i} = std(abs(targetFluxDistribution_f - targetFluxDistribution_b));
-
-    clear targetFluxDistribution_f targetFluxDistribution_b
-end
-
-tmp_table = cell2table([forward_cells, forward_rxns, meanFlux, stdFlux]);
-tmp_table.Properties.VariableNames = {'RxnIndex', 'RxnID', 'meanFlux', 'stdFlux'};
-
-writetable(tmp_table, 'Results/flux_sampling/mixo_sampling_re.csv');
--- Code/matlab/run_FVA.m
+++ Code/matlab/run_FVA.m
@@ -2,34 +2,28 @@ changeCobraSolver('gurobi','all');
 changeCobraSolverParams('LP', 'feasTol', 1e-6);
 changeCobraSolverParams('QP', 'feasTol', 1e-6);
 
+ncpu = 20;
+if isempty(gcp('nocreate'))
+    parpool(ncpu);
+    parfevalOnAll(@maxNumCompThreads, 0, 1); 
+end
+
 %% load different type of models
-o_auto_model = readCbModel('Data/pciCre1355/NDLadpraw_Autotrophic_Rep1.xml');
-o_mixo_model = readCbModel('Data/pciCre1355/NDLadpraw_Mixotrophic_Rep1.xml');
-o_hetero_model = readCbModel('Data/pciCre1355/NDLadpraw_Heterotrophic_Rep1.xml');
+auto_model   = readCbModel('Data/pciCre1355/NDLadpraw_Autotrophic_Rep1.xml');
+mixo_model   = readCbModel('Data/pciCre1355/NDLadpraw_Mixotrophic_Rep1.xml');
+hetero_model = readCbModel('Data/pciCre1355/NDLadpraw_Heterotrophic_Rep1.xml');
 
-auto_model = o_auto_model;
-mixo_model = o_mixo_model;
-hetero_model = o_hetero_model;
-
-% find exchange and uptake reactions
 ex_rxns = auto_model.rxns(findExcRxns(auto_model));
 
-% target only uptake reactions
 upt_rxns = {};
-
 for i = 1:numel(ex_rxns)
-
-    col = find(ismember(auto_model.rxns,ex_rxns{i}));
-
-    % also remove the demand reactions from the list
-    if sum(auto_model.S(:,col)) == 1 & ~contains(ex_rxns{i}, 'DM') & contains(ex_rxns{i}, 'EX')
-
-        upt_rxns = [upt_rxns; ex_rxns(i)];
-
-    elseif sum(auto_model.S(:,col)) > 1
-
-        fprintf('Need to check the stoichiometric matrix for col: %d\n',col)
-        return
+    col = find(strcmp(auto_model.rxns, ex_rxns{i}));
+    
+    % Use nnz (number of non-zero elements) to mathematically check for exchanges
+    if nnz(auto_model.S(:,col)) == 1 && startsWith(ex_rxns{i}, 'EX_') && ~startsWith(ex_rxns{i}, 'DM_')
+        upt_rxns{end+1, 1} = ex_rxns{i};
+    elseif nnz(auto_model.S(:,col)) > 1
+        fprintf('Warning: Need to check the stoichiometric matrix for col: %d (%s)\n', col, ex_rxns{i});
     end
 end
 
@@ -39,19 +33,9 @@ hetero_upt_id = find(ismember(hetero_model.rxns, upt_rxns));
 mixo_upt_id = find(ismember(mixo_model.rxns, upt_rxns));
 
 % remove the upper bound constraint of uptake reactions
-for i = 1:numel(upt_rxns)
-    if auto_model.ub(auto_upt_id(i)) ~= 0
-        auto_model.ub(auto_upt_id(i)) = 1000;
-    end
-
-    if hetero_model.ub(hetero_upt_id(i)) ~= 0
-        hetero_model.ub(hetero_upt_id(i)) = 1000;
-    end
-
-    if mixo_model.ub(mixo_upt_id(i)) ~= 0
-        mixo_model.ub(mixo_upt_id(i)) = 1000;
-    end
-end
+auto_model.ub(auto_upt_id(auto_model.ub(auto_upt_id) ~= 0)) = 1000;
+hetero_model.ub(hetero_upt_id(hetero_model.ub(hetero_upt_id) ~= 0)) = 1000;
+mixo_model.ub(mixo_upt_id(mixo_model.ub(mixo_upt_id) ~= 0)) = 1000;
 
 % find the metabolic reactions
 filterRxns = @(model) model.rxns(~contains(model.rxns, {'EX_','draw','prot_pool'}));
@@ -63,10 +47,6 @@ hetero_rxns = filterRxns(hetero_model);
 rxn_list = intersect(intersect(auto_rxns, mixo_rxns), hetero_rxns);
 
 %% run FVA
-ncpu = 20;
-parpool(ncpu);
-parfevalOnAll(@maxNumCompThreads, 0, 1); 
-
 % FVA analysis
 [auto_minFlux, auto_maxFlux] = FVA_analysis (auto_model, 10, rxn_list);
 [mixo_minFlux, mixo_maxFlux]  = FVA_analysis (mixo_model, 10, rxn_list);
